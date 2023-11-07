e2e2bafda70fc962a9429f391c0c3a90ddc44987
maruf02 <rashiduzzaman99@gmail.com>
2023-11-07 15:16:54 +0600

    Note: Mongodb connection, mongodb env variable and room category post methos is complete

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ofnl5ln.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const AllCarRoomCategoryCollection = client.db('HotelBooking').collection('Room_Category');
+
+        // All Create api works here
+        // All Room Category section
+        app.get('/roomCategory', async (req, res) => {
+            const cursor = AllCarRoomCategoryCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/roomCategory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await AllCarRoomCategoryCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/roomCategory', async (req, res) => {
+            const newBrand = req.body;
+            console.log(newBrand);
+            const result = await AllCarRoomCategoryCollection.insertOne(newBrand);
+            res.send(result);
+        })
+
+        app.delete('/roomCategory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await AllCarRoomCategoryCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // All Room Category section
+
+
+        // All Create api works here
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+// Start main Server
+app.get('/', (req, res) => {
+    res.send('server is running');
+});
+
+app.listen(port, () => {
+    console.log(`server is running in port: ${port}`);
+})
